1fe7bfdad62299e513e8f43ee326b93506be8f9c
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-07 18:34:26 +0100

    Update UBR for Windows 10

--- Modules/WiSeMoUI.psm1
+++ Modules/WiSeMoUI.psm1
@@ -150,8 +150,8 @@ Function SWMB_GetOSVersionColor {
 		@{Os = '11'; DisplayVersion = '23H2'; MajorMinor = '10.0'; Build = 22631; UBR = 6345; Level = 0} # https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information
 		@{Os = '11'; DisplayVersion = '23H2'; MajorMinor = '10.0'; Build = 22631; UBR = 6276; Level = 1} # https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information
 		@{Os = '11'; DisplayVersion = '22H2'; MajorMinor = '10.0'; Build = 22621; UBR = 6060; Level = 3} # https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information
-		@{Os = '10'; DisplayVersion = '22H2'; MajorMinor = '10.0'; Build = 19045; UBR = 6691; Level = 3} # https://learn.microsoft.com/en-us/windows/release-health/release-information
-		@{Os = '10'; DisplayVersion = '21H2'; MajorMinor = '10.0'; Build = 19044; UBR = 6691; Level = 3} # https://learn.microsoft.com/en-us/windows/release-health/release-information
+		@{Os = '10'; DisplayVersion = '22H2'; MajorMinor = '10.0'; Build = 19045; UBR = 6693; Level = 3} # https://learn.microsoft.com/en-us/windows/release-health/release-information
+		@{Os = '10'; DisplayVersion = '21H2'; MajorMinor = '10.0'; Build = 19044; UBR = 6693; Level = 3} # https://learn.microsoft.com/en-us/windows/release-health/release-information
 	)
 
 	$Color = @("Green", "Yellow", "Orange", "Red")
