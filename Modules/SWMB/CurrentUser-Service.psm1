d0f4c39513e5ba7be0c4fb090e3a9a1bf871073a
Gabriel Moreau <gabriel.moreau@univ-grenoble-alpes.fr>
2026-01-15 18:16:19 +0100

    Add tweak TweakViewAutoplay_CU

--- Modules/SWMB/CurrentUser-Service.psm1
+++ Modules/SWMB/CurrentUser-Service.psm1
@@ -43,6 +43,7 @@ Function TweakDisableClipboardHistory_CU {
 
 ################################################################
 
+# A priori obsolete, see NoDriveTypeAutoRun
 # Disable Autoplay
 Function TweakDisableAutoplay_CU {
 	Write-Output "Disabling Autoplay for CU..."
@@ -53,6 +54,22 @@ Function TweakDisableAutoplay_CU {
 Function TweakEnableAutoplay_CU {
 	Write-Output "Enabling Autoplay for CU..."
 	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Type DWord -Value 0
+}
+
+# View
+Function TweakViewAutoplay_CU { # RESINFO
+	Write-Output "Viewing Autoplay for CU (0 or not exist: Enable, 1: Disable)..."
+	$RegPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers'
+	$RegFields = @("DisableAutoplay")
+
+	$Props = Get-ItemProperty -Path $RegPath
+	ForEach ($Field in $RegFields) {
+		If ($Props.PSObject.Properties.Name -notcontains $Field) {
+			Write-Output " ${Field}: not exist"
+			Continue
+		}
+		Write-Output " ${Field}: $($Props.$Field)"
+	}
 }
 
 ################################################################
